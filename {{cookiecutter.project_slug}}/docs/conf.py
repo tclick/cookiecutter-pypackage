ff8889533b6beb79e940a222a7d643714f20ee29
Timothy Click <tclick@alumni.ou.edu>
2017-01-13 17:23:13 +0800

    Added missing variables to docs/conf.py

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -18,6 +18,9 @@ import sys
 import os
 
 # General information about the project.
+project = '{{ cookiecutter.project_slug }}'
+copyright = '{{ cookiecutter.year }}, {{ cookiecutter.full_name }}'
+
 currentdir = os.path.abspath(os.path.dirname(__file__))
 ver_file = os.path.join(currentdir, '..', project, 'version.py')
 with open(ver_file) as f:
